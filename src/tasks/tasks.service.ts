a8e348caa7bc6129e1776775df227809aee5c0b3
Klismann de Oliveira Barros <klismannbarros@eng.ci.ufpb.br>
2022-08-17 19:36:54 -0300

    Added createTask method to service tasks

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,10 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { Task, TaskStatus } from './task.model';
+import { v4 as uuid } from 'uuid';
 
 @Injectable()
 export class TasksService {
-  private tasks = [];
+  private tasks: Task[] = [];
 
-  public getAllTasks() {
+  public getAllTasks(): Task[] {
     return this.tasks;
+  }
+
+  public createTask(title: string, description: string): Task {
+    const task: Task = {
+      id: uuid(),
+      title: title,
+      description: description,
+      status: TaskStatus.OPEN,
+    };
+
+    this.tasks.push(task);
+
+    return task;
   }
 }
