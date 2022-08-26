26ee03962e893c3709d65bd18d0065914ca1465e
Klismann de Oliveira Barros <klismannbarros@eng.ci.ufpb.br>
2022-08-26 10:39:44 -0300

    Added user entity to the Auth module

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { AuthCredentialsDTO } from './dto/auth-credentials.dto';
+import { User } from './user.entity';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  @InjectRepository(User)
+  private usersRepository: Repository<User>;
+
+  async createUser(authCredentialsDTO: AuthCredentialsDTO): Promise<void> {
+    const { username, password } = authCredentialsDTO;
+
+    const user = this.usersRepository.create({ username, password });
+    await this.usersRepository.save(user);
+  }
+}
