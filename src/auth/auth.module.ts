93396a03441058739958af1a241ff6579de849bb
Klismann de Oliveira Barros <klismannbarros@eng.ci.ufpb.br>
2022-11-17 16:16:32 -0300

    Added config service to handle the JWT Secret to avoid hard coded insertion

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,15 +6,18 @@ import { User } from './user.entity';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './jwt.strategy';
+import { ConfigService, ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     PassportModule.register({ defaultStrategy: 'jwt' }),
-    JwtModule.register({
-      secret: 'topSecret51',
-      signOptions: {
-        expiresIn: 3600,
-      },
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET'),
+        signOptions: { expiresIn: '1h' },
+      }),
     }),
     TypeOrmModule.forFeature([User]),
   ],
